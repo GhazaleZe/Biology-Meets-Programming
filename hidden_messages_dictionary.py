87f4d4de449f277637ff80d2a985f6bad7945d72
ghazale <ghazalze@yahoo.com>
2020-03-21 00:56:32 +0330

    print the most frequent patterns

--- hidden_messages_dictionary.py
+++ hidden_messages_dictionary.py
@@ -1,11 +1,23 @@
+def FrequentWords(Text, k):
+    words = []
+    freq = FrequencyMap(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+    return words
+
+
 def FrequencyMap(Text, k):
     freq = {}
     n = len(Text)
-    for i in range(n-k+1):
-        Pattern = Text[i:i+k]
+    for i in range(n - k + 1):
+        Pattern = Text[i:i + k]
         freq[Pattern] = 0
-    for i in range(n-k+1):
-        Pattern = Text[i:i+k]
-        freq[Pattern] +=1
+    for i in range(n - k + 1):
+        Pattern = Text[i:i + k]
+        freq[Pattern] += 1
     return freq
-print(FrequencyMap("CGATATATCCATAG", 3))
+
+
+print(FrequentWords("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4))
