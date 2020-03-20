d7699b64e655746e9c97084b5e39ffff65dc4ff8
ghazale <ghazalze@yahoo.com>
2020-03-21 00:36:15 +0330

    count each string of length k

--- hidden_messages_dictionary.py
+++ hidden_messages_dictionary.py
@@ -4,4 +4,8 @@ def FrequencyMap(Text, k):
     for i in range(n-k+1):
         Pattern = Text[i:i+k]
         freq[Pattern] = 0
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        freq[Pattern] +=1
     return freq
+print(FrequencyMap("CGATATATCCATAG", 3))
