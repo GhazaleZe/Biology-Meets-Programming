835b733b7afb5f492df54e2d356e120e8f3ce913
ghazale <ghazalze@yahoo.com>
2020-03-21 14:13:12 +0430

    Complement function is added

--- ReverseComplement.py
+++ ReverseComplement.py
@@ -1,4 +1,17 @@
 def Reverse(Pattern):
     new= Pattern[::-1]
     return new
+def Complement(Pattern):
+    new = ""
+    for c in Pattern:
+        if c == 'A':
+            new += 'T'
+        elif c == 'T':
+            new += 'A'
+        elif c == 'G':
+            new += 'C'
+        elif c == 'C':
+            new += 'G'
+    return new
 print(Reverse("AAAACCCGGT"))
+print(Complement("AAAACCCGGT"))
