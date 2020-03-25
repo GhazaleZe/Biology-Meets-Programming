31dfe6cdd70bd8352a07e4cf54245a29032179fb
ghazale <ghazalze@yahoo.com>
2020-03-26 01:11:19 +0430

    generate a count matrix from an arbitrary list of strings Motifs

---
+++ Motifs.py
@@ -0,0 +1,13 @@
+def Count(Motifs):
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]  # range over all elements symbol = Motifs[i][j] of the count matrix
+            count[symbol][j] += 1  # add 1 to count[symbol][j].
+    return count
