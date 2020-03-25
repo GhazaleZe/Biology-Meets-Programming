4caaf06e69026d6d68bd6ae6ca1cd03172e0fee7
ghazale <ghazalze@yahoo.com>
2020-03-26 01:32:36 +0430

    Profile(Motifs) is added
    divide each element of the count matrix by the number of rows in the count matrix

--- Motifs.py
+++ Motifs.py
@@ -11,3 +11,18 @@ def Count(Motifs):
             symbol = Motifs[i][j]  # range over all elements symbol = Motifs[i][j] of the count matrix
             count[symbol][j] += 1  # add 1 to count[symbol][j].
     return count
+
+
+def Profile(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    profile = Count(Motifs)
+    for symbol in "ACGT":
+        for c in range(k):
+            profile[symbol][c] /= t
+    return profile
+
+
+a = []
+a = ["AACGTA", "CCCGTT", "CACCTT", "GGATTA", "TTCCGG"]
+print(Profile(a))
