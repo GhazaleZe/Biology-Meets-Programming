5fde43d557e7c9af57023b134a4f06dfd519bb54
ghazale <ghazalze@yahoo.com>
2020-03-28 20:24:15 +0430

    while ture function
    generating a collection of random motifs

---
+++ RandomizedMotifSearch.py
@@ -0,0 +1,122 @@
+import random
+
+
+def RandomizedMotifSearch(Dna, k, t):
+
+    M = RandomMotifs(Dna, k, t)
+    BestMotifs = M
+    while True:
+        Profile = ProfileWithPseudocounts(M)
+        M = Motifs(Profile, Dna)
+        if Score(M) < Score(BestMotifs):
+            BestMotifs = M
+        else:
+            return BestMotifs
+
+
+def RandomMotifs(Dna, k, t):
+    col = len(Dna[0])
+    s = []
+    for i in range(t):
+        new = random.randint(0, (col - k))
+        s.append(Dna[i][new:new + k])
+    return s
+
+
+def CountWithPseudocounts(Motifs):
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(1)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]  # range over all elements symbol = Motifs[i][j] of the count matrix
+            count[symbol][j] += 1  # add 1 to count[symbol][j].
+    return count
+
+
+def ProfileWithPseudocounts(Motifs):
+    t = len(Motifs)
+    t += 4
+    k = len(Motifs[0])
+    profile = CountWithPseudocounts(Motifs)
+    for symbol in "ACGT":
+        for c in range(k):
+            profile[symbol][c] /= t
+    return profile
+
+def Pr(Text, Profile):
+    p = 1
+    for i in range(len(Text)):
+        p = p * Profile[Text[i]][i]
+    return p
+
+
+def ProfileMostProbableKmer(Text, k, Profile):
+    n = len(Text)
+    goal = Text[0:k]
+    m = 0
+    for i in range(n - k + 1):
+        val = Pr(Text[i:i + k], Profile)
+        if val > m:
+            m = val
+            goal = Text[i:i + k]
+    return goal
+
+
+def Motifs(Profile, Dna):
+    k = len(Profile['A'])
+    n = len(Dna)
+    my_list = []
+    for i in range(n):
+        new = ProfileMostProbableKmer(Dna[i], k, Profile)
+        my_list.append(new)
+    return my_list
+def Count(Motifs):
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]  # range over all elements symbol = Motifs[i][j] of the count matrix
+            count[symbol][j] += 1  # add 1 to count[symbol][j].
+    return count
+
+
+def Consensus(Motifs):
+    consensus = ""
+    count = Count(Motifs)
+    k = len(Motifs[0])
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
+
+
+def Score(Motifs):
+    count = Count(Motifs)
+    consensus = Consensus(Motifs)
+    score = 0
+    i = 0
+    r = len(Motifs)
+    for char in consensus:
+        p = count[char][i]
+        x = r - p
+        score += x
+        i = i+1
+    return score
+
+dna = ["CGCCCCTCTCGGGGGTGTTCAGTAAACGGCCA", "GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG", "TAGTACCGAGACCGAAAGAAGTATACAGGCGT", "TAGATCAAGTTTCAGGTGCACGTCGGTGAACC", "AATCCACCAGCTCCACGTGCAATGTTGGCCTA"]
+print(RandomizedMotifSearch(dna, 8, 5))
